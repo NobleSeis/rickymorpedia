02552acc0b1fb4457a3ac8297e9550a32bff0986
Jose Manuel <jose.gisla@gmail.com>
2023-04-17 23:56:40 +0200

    [Todo] Dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,20 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { PersonajeService } from '../services/personaje.service';
-import { Personaje } from '../models/personaje';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent implements OnInit {
-  personajes: Personaje[] = [];
-
-  constructor(private personajeService: PersonajeService) {}
-
-  ngOnInit(): void {
-    this.personajeService
-      .getAll()
-      .subscribe((data: any) => (this.personajes = data.results));
-  }
-}
+export class DashboardComponent {}
